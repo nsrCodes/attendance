9df0a002416f5ec15564da43e0645b023ebafc8b
Navdeep Singh Rathore <razor@pop-os.localdomain>
2020-08-30 10:44:03 +0530

    Updates

---
+++ precache-manifest.6b82ed55a014e3f1bc60d5415fdfdaae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f209035311831ceee89becde173c2464",
+    "url": "/attendance/index.html"
+  },
+  {
+    "revision": "7fceacb8e3c595a83e82",
+    "url": "/attendance/static/css/main.60c5ff46.chunk.css"
+  },
+  {
+    "revision": "78e8be3fff757d51d9f0",
+    "url": "/attendance/static/js/2.7a1ff6e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/attendance/static/js/2.7a1ff6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fceacb8e3c595a83e82",
+    "url": "/attendance/static/js/main.efb320a2.chunk.js"
+  },
+  {
+    "revision": "cb7728c63f37cbf905d4",
+    "url": "/attendance/static/js/runtime-main.b40ed504.js"
+  }
+]);
